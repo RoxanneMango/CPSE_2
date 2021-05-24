e9a87598afe97776f96fff0fc43200dbc106dd64
RoxanneMango <roxannevdpol@gmail.com>
2021-05-24 17:12:10 +0200

    ++tic_tac_toe

---
+++ 5_tic_tac_toe/tic_tac_toe.h
@@ -0,0 +1,257 @@
+#ifndef TIC_TAC_TOE_H
+#define TIC_TAC_TOE_H
+
+#include "stdio.h"
+
+#include <vector>
+#include <string>
+
+/* * * * * * * * * * *
+*
+*	Tic-tac-toe board
+*
+*	 O | X | X 
+*	---|---|---
+*	 X | O | X
+*	---|---|---
+*	 X | O | O
+*
+* * * * * * * * * * */
+
+typedef struct Command
+{
+	bool isPlayer1;
+	unsigned int pos;
+} Command;
+
+class TicTacToe
+{
+protected:
+	static constexpr unsigned int WIDTH = 12;
+	static constexpr unsigned int HEIGHT = 5;
+
+	unsigned int x = 0;
+	unsigned int y = 0;
+	unsigned int numOfUndos = 0;
+	unsigned int maxNumOfUndos = 1;
+	
+	char input;
+	char player1;
+	char player2;
+
+	bool error = false;
+	bool isEnd = false;
+	bool isWinner = false;
+	bool player1Turn = true;
+	bool isKeyPressed = false;
+
+	std::string board = {"   |   |   \n---|---|---\n   |   |   \n---|---|---\n   |   |   \n"};
+	std::string topText = {"Controls:\ntop row: 1,2,3; mid row: 4, 5, 6; bottom row: 7,8,9; quit: q; undo: u;\n"};
+	std::string errorText;
+	std::string bottomText;
+	
+	std::vector<Command> commandList;
+	
+	virtual void getInput()
+	{
+		isKeyPressed = true;
+		input = getchar();
+		for(int c; (c = getchar()) != EOF && c != '\n';);
+	}
+	
+	bool isWon()
+	{
+		// Check if there are enough turns to have a winner
+		if(commandList.size() > 4)
+		{
+			// Recreate the board and populate it with values that are not '1' or '0'; doesn't really matter which number
+			// in this case it's the number 3.
+			// manually filled in since memset() would require the #include'sion of <cstring> or "string.h" and it feels
+			// unnecessary to include it solely for that
+			int board[3 * 3] = {3,3,3,3,3,3,3,3,3};
+			
+			// player1Turn gets flipped before the isWon calculation; this flips it back
+			int turn = 1 - player1Turn;
+			
+			// populate dummy board with the turns from commandList
+			for(unsigned int i = 0; i < commandList.size(); ++i)
+			{
+				int x = ( (commandList[i].pos % WIDTH - 1) / 3 );
+				int y = (commandList[i].pos / WIDTH) ? (commandList[i].pos / WIDTH) / 2 : 0;
+				board[(y * 3) + x] = commandList[i].isPlayer1;
+			}
+
+			// set initial win condition to true
+			bool isSame = true;
+			
+			// check for horizontal wins
+			for(unsigned int y = 0; y < 3; ++y)
+			{
+				// new row, new chances
+				isSame = true;
+				for(unsigned int x = 0; (x < 3) && isSame; ++x)
+				{
+					// if the pattern is broken, go back to previous loop to start over
+					// else keep counting and if there's 3 in a row, return true
+					if(board[(y * 3) + x] != turn) isSame = false;
+					else if(x >= 2) return true;
+				}
+			}
+			// vertical
+			for(unsigned int x = 0; x < 3; ++x)
+			{
+				isSame = true;
+				for(unsigned int y = 0; (y < 3) && isSame; ++y)
+				{
+					if(board[(y * 3) + x] != turn) isSame = false;
+					else if(y >= 2) return true;
+				}
+			}
+			// check for three in a row diagonally
+			// Too smol brain for fancy for loop construction; quick 'n dirty logic sausage monstrosity...
+			if((board[4] == turn) && (((board[0] == turn) && (board[8] == turn)) || ((board[2] == turn) && (board[6] == turn))))
+			{
+				return true;
+			}
+		}
+		return false;
+	}
+	
+public:
+	bool isRunning = true;
+
+	TicTacToe(char p1, char p2) : player1(p1), player2(p2), bottomText("player " + std::to_string(2-player1Turn) + " (" + (player1Turn ? player1 : player2) + ") input: ")
+	{}
+	virtual ~TicTacToe()
+	{}
+	
+	virtual void handleInput()
+	{
+		// skip this function if the game is already over; avoid stalling due to getchar() waiting for input
+		if(isEnd || (commandList.size() >= 9)) return;
+		
+		getInput();
+		
+		// only handle input if there is input
+		if(isKeyPressed)
+		{
+			try
+			{	
+				// reset error flag
+				error = false;
+				
+				// quit
+				if(input == 'q')
+				{
+					isRunning = false;
+					return;
+				}
+				
+				// undo
+				if(input == 'u')
+				{
+					if( (commandList.size() > 0) && (numOfUndos < maxNumOfUndos) )
+					{
+						commandList.pop_back();
+						numOfUndos++;
+						
+						// flip player turn back to previous player
+						player1Turn = player1Turn ? 0 : 1;
+					}
+					else
+					{
+						throw "could not undo";
+					}
+					return;
+				}
+				
+				// convert char input to decimal number
+				input = input - '0';
+				if((input < 1) || (input > 9)) throw "invalid input";
+				
+				// calculate the position within the board
+				x = ((input-1)%3);
+				y = input <= 3 ? 0 : input <= 6 ? 2 : 4;
+				unsigned int pos = (y * WIDTH) + (x + (x * 3) + 1);
+				
+				// only touch it if it's not empty
+				if(commandList.size() > 0)
+				{
+					// check if the space is already occupied, if so, throw an error message which will be displayed
+					// to the user
+					for(unsigned int i = 0; i < commandList.size(); ++i)
+					{
+						if(commandList[i].pos == pos) throw "space is already occupied";
+					}
+				}
+				// if the move is valid (no invalid input or occupied spaces), push a new Command to the commandList
+				// and reset the number of undos
+				commandList.push_back(Command{player1Turn, pos});
+				numOfUndos = 0;
+			}
+			catch(const char * exception)
+			{
+				// an error occured: set error flag to true and populate errorText with the error that was thrown
+				error = true;
+				errorText.assign(std::string(exception) + " . . .\n\n-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n\n");
+			}
+		}
+	}
+	
+	virtual void update()
+	{
+		// once the last cycle has been done to draw the final state of the board, the game will stop by setting
+		// isRunning to false and returning
+		if(isEnd)
+		{
+			isRunning = false;
+			return;
+		}
+		
+		// only update if something changed (a key was pressed)
+		if(isKeyPressed)
+		{
+			// Build the board based on the moves saved in commandList
+			board.assign("   |   |   \n---|---|---\n   |   |   \n---|---|---\n   |   |   \n");
+			for(unsigned int i = 0; i < commandList.size(); ++i)
+			{
+				board[commandList[i].pos] = commandList[i].isPlayer1 ? player1 : player2;
+			}
+			
+			// flip player turn if the turn was valid and wasn't an undo
+			if(isRunning && !error && input != 'u')
+			{	
+				player1Turn = player1Turn ? false : true;
+			}
+
+			bottomText.assign("player " + std::to_string(2-player1Turn) + " (" + (player1Turn ? player1 : player2) + ") input: ");
+			if((isWinner = isWon()) || commandList.size() >= 9)
+			{
+				if(isWinner)
+				{
+					bottomText.assign("player " + std::to_string(player1Turn+1) + " has won!");
+				}
+				else
+				{
+					bottomText.assign("game ended in a tie!");
+				}
+				// flag signifies the end but allows for one more cycle of drawing the board in text mode
+				// game would end prematurely if isRunning was set to false here directly.
+				isEnd = true;
+			}
+			isKeyPressed = false;
+		}
+	}
+	
+	virtual void draw()
+	{
+		printf("\n%s%s%s%s", error ? errorText.c_str() : "", topText.c_str(), board.c_str(), bottomText.c_str());
+	}
+	
+	virtual void clear()
+	{
+		printf("\n-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
+	}
+};
+
+#endif // TIC_TAC_TOE_H
---
+++ 5_tic_tac_toe/tic_tac_toe_GUI.h
@@ -0,0 +1,132 @@
+#ifndef TIC_TAC_TOE_GUI
+#define TIC_TAC_TOE_GUI
+
+#include <SFML/Graphics.hpp>
+
+#include "tic_tac_toe.h"
+
+class TicTacToeGUI : public TicTacToe
+{
+private:
+	sf::RenderWindow window;
+	sf::View view;
+	sf::Event event;
+	
+	sf::Font font;
+	
+	sf::Text board_sf;
+	sf::Text topText_sf;
+	sf::Text errorText_sf;
+	sf::Text bottomText_sf;
+
+	virtual void getInput() override
+	{
+		// set initial input to 0
+		input = 0;
+		while(window.pollEvent(event))
+		{
+			if(event.type == sf::Event::Closed)
+			{
+				window.close();
+			}
+			if(event.type == sf::Event::TextEntered)
+			{
+				// check if input falls within the ASCII range
+				if (event.text.unicode < 128)
+				{
+					input = event.text.unicode;
+				}
+				if(isEnd)
+				{
+					isRunning = false;
+				}
+				isKeyPressed = true;
+			}
+		}
+	}
+	
+public:
+	TicTacToeGUI(char p1, char p2) : 
+		window(sf::VideoMode(720, 480), "CPSE2_5", sf::Style::Close), 
+		view(sf::Vector2f(0.0f,0.0f), sf::Vector2f(720, 480)),
+		TicTacToe(p1, p2),
+		board_sf(board, font, 16),
+		topText_sf(topText, font, 16),
+		errorText_sf(errorText, font, 16),
+		bottomText_sf(bottomText, font, 16)
+	{
+		window.setFramerateLimit(60);
+		
+		// if the font could not be loaded, just exit with error code -1
+		if(!font.loadFromFile("Consolas.ttf")) exit(-1);
+		
+		// position the text fields
+		topText_sf.setPosition(sf::Vector2f(0,0));
+		board_sf.setPosition(sf::Vector2f(0, 3*16));
+		bottomText_sf.setPosition(sf::Vector2f(0, 10*16));
+		errorText_sf.setPosition(sf::Vector2f(0, 12*16));
+	}
+	virtual ~TicTacToeGUI()
+	{}
+
+	virtual void update() override
+	{
+		// if its the end, handle last input; getInput() will set isRunning to false, thus, ending the game.
+		// this is to prevent the game from immediately exiting after the game ended, to allow the player(s)
+		// to see the final score
+		if(isEnd)
+		{
+			getInput();
+		}
+		else
+		{
+			// only update if something changed
+			if(isKeyPressed)
+			{
+				// Build the board based on the moves saved in commandList
+				board.assign("   |   |   \n---|---|---\n   |   |   \n---|---|---\n   |   |   \n");
+				for(unsigned int i = 0; i < commandList.size(); ++i)
+				{
+					board[commandList[i].pos] = commandList[i].isPlayer1 ? player1 : player2;
+				}
+				
+				// flip player turn if the turn was valid and wasn't an undo
+				if(isRunning && !error && input != 'u')
+				{	
+					player1Turn = player1Turn ? false : true;
+				}
+
+				bottomText_sf.setString("player " + std::to_string(2 - player1Turn) + " (" + std::string(1, player1Turn ? player1 : player2) + ") input: ");
+				if((isWinner = isWon()) || commandList.size() >= 9)
+				{
+					bottomText_sf.setString(isWinner ? std::string("player " + std::to_string(player1Turn+1) + " has won!") : std::string("game ended in a tie!"));
+					isEnd = true;
+					error = true;
+					errorText.assign("press any key to exit . . .\n\n-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n\n");
+				}
+				board_sf.setString(board);
+				errorText_sf.setString(errorText);				
+				isKeyPressed = false;
+			}
+		}
+		// if the window is no longer open, stop the game
+		if(!window.isOpen()) isRunning = false;
+	}
+	
+	virtual void draw() override
+	{
+		window.draw(topText_sf);
+		window.draw(board_sf);
+		window.draw(bottomText_sf);
+		// only draw error text is an error occured
+		if(error) window.draw(errorText_sf);
+		window.display();
+	}
+	
+	virtual void clear() override
+	{
+		window.clear(sf::Color::Black);
+	}
+};
+
+#endif // TIC_TAC_TOE_GUI
